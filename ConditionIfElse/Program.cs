39063e2ef8ec3237e925d320792f9f9399b1af76
kudesn1k02 <pavelpauk666@gmail.com>
2023-02-16 23:35:43 +0300

    add Condition If Else project

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя... ");
+string username = Console.ReadLine();
+
+if(username.ToLover() == "Василий")
+{
+    Console.Write("Ошалееееть! Это же Василий!!!" );
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
